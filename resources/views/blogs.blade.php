4da4dbcdc50da47851cbfe0285ae78f5b7ab1775
Phyo-Zayar-Lin <pzayarlin6@gmail.com>
2025-06-21 09:08:23 +0630

    first

---
+++ resources/views/blogs.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>blog</title>
+
+    <link rel="stylesheet" href="/css/app.css">
+    <!-- <script src="/app.js"></script> -->
+
+</head>
+
+<body>
+    <article>
+        <h1><a href="/blogs/first-blog">First Blog</a></h1>
+        <P>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat beatae placeat temporibus, deleniti,
+            aliquid fugit nesciunt ipsam culpa deserunt vero aspernatur quisquam dolore impedit error similique omnis
+            ducimus cumque. Iste!
+        </P>
+    </article>
+    <article>
+        <h1><a href="/blogs/second-blog">second Blog</a></h1>
+        <P>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat beatae placeat temporibus, deleniti,
+            aliquid fugit nesciunt ipsam culpa deserunt vero aspernatur quisquam dolore impedit error similique omnis
+            ducimus cumque. Iste!
+        </P>
+    </article>
+    <article>
+        <h1><a href="/blogs/third-blog">Third Blog</a></h1>
+        <P>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat beatae placeat temporibus, deleniti,
+            aliquid fugit nesciunt ipsam culpa deserunt vero aspernatur quisquam dolore impedit error similique omnis
+            ducimus cumque. Iste!
+        </P>
+    </article>
+</body>
+
+</html>
