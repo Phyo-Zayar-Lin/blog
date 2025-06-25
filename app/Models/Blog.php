ec79fc5f03b3c9e24cd9a7d79040f917c03db1d9
Phyo-Zayar-Lin <pzayarlin6@gmail.com>
2025-06-26 06:28:22 +0630

    ep11 composer parse package

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -2,8 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Facades\File;
 
 class Blog{
+    public static function all(){
+       $files = File::files(resource_path("blogs"));
+        return array_map(function($file){
+            return $file->getContents();
+            // echo "<pre>";
+            // var_dump($file);
+        },$files);
+        // dd($files[0]->getContents());
+    }
     public static function find($slug){
     $path=resource_path("blogs/$slug.html");
     if(!file_exists($path)){
--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -12,30 +12,9 @@
 </head>
 
 <body>
-    <article>
-        <h1><a href="/blogs/first-blog">First Blog</a></h1>
-        <P>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat beatae placeat temporibus, deleniti,
-            aliquid fugit nesciunt ipsam culpa deserunt vero aspernatur quisquam dolore impedit error similique omnis
-            ducimus cumque. Iste!
-        </P>
-    </article>
-    <article>
-        <h1><a href="/blogs/second-blog">second Blog</a></h1>
-        <P>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat beatae placeat temporibus, deleniti,
-            aliquid fugit nesciunt ipsam culpa deserunt vero aspernatur quisquam dolore impedit error similique omnis
-            ducimus cumque. Iste!
-        </P>
-    </article>
-    <article>
-        <h1><a href="/blogs/third-blog">Third Blog</a></h1>
-        <P>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat beatae placeat temporibus, deleniti,
-            aliquid fugit nesciunt ipsam culpa deserunt vero aspernatur quisquam dolore impedit error similique omnis
-            ducimus cumque. Iste!
-        </P>
-    </article>
+    <?php foreach($blogs as $blog):?>
+    <?= $blog; ?>
+    <?php endforeach;?>
 </body>
 
 </html>
