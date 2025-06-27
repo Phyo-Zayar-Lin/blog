80350cf4bd993658525acc3fac27a2b45de55c82
Phyo-Zayar-Lin <pzayarlin6@gmail.com>
2025-06-27 07:09:09 +0630

    ep12 YFMatter

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -3,16 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Blog{
+    public $title;
+    public $slug;
+    public $intro;
+    public $body;
+    public function __construct($title,$slug,$intro,$body){
+        $this->title=$title;
+        $this->slug=$slug;
+        $this->intro=$intro;
+        $this->body=$body;
+    }
     public static function all(){
        $files = File::files(resource_path("blogs"));
-        return array_map(function($file){
-            return $file->getContents();
-            // echo "<pre>";
-            // var_dump($file);
-        },$files);
-        // dd($files[0]->getContents());
+       $blogs=[];
+       foreach($files as $file){
+        $obj = YamlFrontMatter :: parseFile($file);
+        $blog = new Blog($obj->title,$obj->slug,$obj->intro,$obj->body());
+        $blogs[] = $blog;
+       }
+       return $blogs;
+
     }
     public static function find($slug){
     $path=resource_path("blogs/$slug.html");
