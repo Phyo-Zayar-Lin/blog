f652329eb3220b61320176a7e56e6139ab9da700
Phyo-Zayar-Lin <pzayarlin6@gmail.com>
2026-02-03 09:22:54 +0630

    create factroy ch5 ep13

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,32 +19,12 @@ class DatabaseSeeder extends Seeder
         Category::truncate();
         Blog::truncate();
 
-        User::factory(1)->create();
+        $frontend=Category::factory()->create(['name'=>"frontend"]);
+        $backend=Category::factory()->create(['name'=>"backend"]);
 
-        $frontend=Category::create([
-            'name'=>"frontend",
-            'slug'=>"frontend"
-        ]);
-
-        $backend=Category::create([
-            'name'=>"backend",
-            'slug'=>"backend"
-        ]);
-
-        Blog::create([
-            'title'=>"Frontend Post",
-            'slug'=>"Frontend-Post",
-            'intro'=>"this is intro",
-            'body'=>"this is body and Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente laborum sunt corrupti veritatis sit. Error iste praesentium nihil delectus voluptas aliquid vero commodi adipisci deserunt quas. Praesentium dolore placeat mollitia natus obcaecati dicta ipsum veritatis rem nihil, consectetur ullam minima veniam ab odio perferendis illo debitis cum! Doloremque, vero ad?",
-            'category_id'=>$frontend->id
-        ]);
-        Blog::create([
-            'title'=>"Backend Post",
-            'slug'=>"Backend-Post",
-            'intro'=>"this is intro",
-            'body'=>"this is body and Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente laborum sunt corrupti veritatis sit. Error iste praesentium nihil delectus voluptas aliquid vero commodi adipisci deserunt quas. Praesentium dolore placeat mollitia natus obcaecati dicta ipsum veritatis rem nihil, consectetur ullam minima veniam ab odio perferendis illo debitis cum! Doloremque, vero ad?",
-            'category_id'=>$backend->id
-        ]);
+        User::factory()->create();
+        Blog::factory(2)->create(['category_id'=>$frontend->id]);
+        Blog::factory(2)->create(['category_id'=>$backend->id]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
